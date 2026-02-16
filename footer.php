15652be7b888bca76a1f7e98ba7e3cb790c8dbd1
Owel <mbrillantes@ids-inc.net>
2026-02-16 14:54:13 +0800

    Update template

--- footer.php
+++ footer.php
@@ -1,3 +1,17 @@
+<?php 
+/**
+ * This template displays the <footer> section and opening of the body
+ *
+ * PHP version: 8.0+
+ *
+ * @category Theme
+ * @package  Ams_Soviner
+ * @author   Manuel Brillantes II <brillantesmanuel@gmail.com>
+ * @license  https://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
+ * @link     https://brillantesmanuel.website
+ */
+
+?>
 <footer class="bg-dark text-white py-4 mt-5">
 	<div class="container text-center">
 		&copy; 2026 <?php bloginfo( 'name' ); ?>. All Rights Reserved.
